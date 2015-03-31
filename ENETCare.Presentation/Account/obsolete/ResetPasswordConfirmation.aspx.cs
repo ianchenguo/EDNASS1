9eec836713d91240f69b2904bcc1106c1af12b5a
ianchenguo <ianchenguo@gmail.com>
2015-03-31 20:16:13 +1100

    last resort

--- ENETCare.Presentation/Account/obsolete/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace ENETCare.Presentation.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- ENETCare.Presentation/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ENETCare.Presentation.Global" Language="C#" %>
