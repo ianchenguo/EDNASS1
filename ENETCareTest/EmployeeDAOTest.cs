0838afcfc495726548aa07d24c930f6473393677
jeffyao <dabeige@gmail.com>
2015-04-06 19:44:42 +1000

    nunit&moq lib added; All DAO tests except MedicationTypeDAO added.

---
+++ ENETCareTest/EmployeeDAOTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ENETCare.Business;
+
+namespace ENETCareTest
+{
+    [TestClass]
+    public class EmployeeDAOTest
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+         Employee employee = new Employee();
+        }
+
+
+        [TestMethod]
+        public void FindEmployeeByUserName_Test()
+        {
+            var employeeone = new Mock<EmployeeDAO>();
+            employeeone.Setup(x => x.FindEmployeeByUserName("jeff")).Returns(new Employee());
+            employeeone.Object.FindEmployeeByUserName("jeff");
+            Assert.AreNotSame(new Employee(), employeeone.Object.FindEmployeeByUserName("jeff"));
+        }
+    }
+}
---
+++ ENETCareTest/MedicationPackageDAOTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ENETCare.Business;
+using Moq;
+using System.Collections.Generic;
+
+namespace ENETCareTest
+{
+    [TestClass]
+    public class MedicationPackageDAOTest
+    {
+        MedicationPackage package = new MedicationPackage();
+        int distributionCentreId;
+        string barcode;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            
+        }
+
+        [TestMethod]
+        public void InsertPackage_Test()
+        {
+            var insert = new Mock<MedicationPackageDAO>();
+            insert.Setup(x => x.InsertPackage(It.IsAny<MedicationPackage>()));
+            insert.Object.InsertPackage(package);
+            insert.Verify();//check if the method has been called
+        }
+
+        [TestMethod]
+        public void UpdatePackage_Test()
+        {
+            var update = new Mock<MedicationPackageDAO>();
+            update.Setup(x => x.UpdatePackage(It.IsAny<MedicationPackage>()));
+            update.Object.UpdatePackage(package);
+            update.Verify();//check if the method has been called
+        }
+
+        [TestMethod]
+        public void FindAllPackages_Test() 
+        {
+            //test return value of FindAllPackages
+            var findlist = new Mock<MedicationPackageDAO>();
+            findlist.Setup(x => x.FindAllPackages()).Returns(new List<MedicationPackage>());//problem???
+            findlist.Object.FindAllPackages();
+            Assert.IsNotNull(findlist.Object.FindAllPackages());
+        }
+
+        [TestMethod]
+        public void FindPackagesInDistributionCentre_Test()
+        {
+            
+            var findp = new Mock<MedicationPackageDAO>();
+            findp.Setup(x => x.FindPackagesInDistributionCentre(distributionCentreId)).Returns(new List<MedicationPackage>());
+            findp.Object.FindPackagesInDistributionCentre(distributionCentreId);
+            Assert.AreNotSame(new List<MedicationPackage>(), findp.Object.FindPackagesInDistributionCentre(distributionCentreId));
+        }
+
+        [TestMethod]
+        public void FindPackageByBarcode_Test()
+        {
+            
+            var findpb = new Mock<MedicationPackageDAO>();
+            //findpb.Setup(x => x.FindPackageByBarcode(barcode)).Returns(null);
+            findpb.Object.FindPackageByBarcode(barcode);
+            Assert.IsNull(findpb.Object.FindPackageByBarcode(barcode));
+        }
+    }
+}
