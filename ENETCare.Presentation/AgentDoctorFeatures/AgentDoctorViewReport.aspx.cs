392acb0522584410aaeb0816fa4e7d9890ae9869
11582145 Kelin Liao <11582145@uts.student.edu.au>
2015-04-26 15:54:37 +1000

    AgentDoctor[12]

--- ENETCare.Presentation/AgentDoctorFeatures/AgentDoctorViewReport.aspx.cs
+++ ENETCare.Presentation/AgentDoctorFeatures/AgentDoctorViewReport.aspx.cs
@@ -10,6 +10,7 @@ using ENETCare.Presentation.HelperUtilities;
 using System.Data;
 using System.Data.Sql;
 using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace ENETCare.Presentation.AgentDoctorFeatures
 {
@@ -23,6 +24,17 @@ namespace ENETCare.Presentation.AgentDoctorFeatures
             {
                 this.AgentDoctorReportStockTakeDataBind();
             }
+
+            GridViewRowCollection rowsCollection = AgentDoctorReportStockTakingGV.Rows;
+            GridViewRow row = rowsCollection[1];
+            string rowContent1 = row.Cells[0].Text;
+            string rowContent4 = row.Cells[4].Text;
+            int ColumnsCount = AgentDoctorReportStockTakingGV.Columns.Count;
+
+            Debug.WriteLine("\nColumnsCount is -- " + ColumnsCount);
+            Debug.WriteLine("\nRow 1 Cell 0 is -- " + rowContent1);
+            Debug.WriteLine("\nRow 1 Cell 4 is -- " + rowContent4);
+            
         }
 
         private void AgentDoctorReportStockTakeDataBind()
@@ -38,10 +50,14 @@ namespace ENETCare.Presentation.AgentDoctorFeatures
                 AgentDoctorReportStockTaking.DiscardPackage(e.CommandArgument.ToString());
                 this.AgentDoctorReportStockTakeDataBind();
             }
+            //Debug.WriteLine("\nLocalRow ExpiredStatus is: --\n" + ExpiredStatusStrLocalRow);
         }
 
         protected void AgentDoctorReportStockTakingGV_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
+            //int rowIndex = ((GridViewRow)((LinkButton)e.CommandSource).NamingContainer).RowIndex;
+            //string ExpiredStatusStrLocalRow = (AgentDoctorReportStockTakingGV.Rows[rowIndex].Cells[4]).Text;
+            //string theStr = AgentDoctorReportStockTakingGV.Columns[0].HeaderText.ToString();
             AgentDoctorReportStockTakingGV.PageIndex = e.NewPageIndex;
             this.AgentDoctorReportStockTakeDataBind();
         }
